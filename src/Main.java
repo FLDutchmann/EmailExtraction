671e55f6440cee0ec45c65f160e4420e494261d7
ZOO\AreMel <arend.mellendijk@softwire.com>
2021-07-01 11:08:43 +0100

    Switched to regex

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,21 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String args[]) throws IOException {
         Path filePath = Path.of("sample.txt");
         String input = Files.readString(filePath);
+        Pattern pattern = Pattern.compile("[ \n][a-zA-z.'_%+\\-]+@softwire\\.com[ \n]");
+        Matcher matcher = pattern.matcher(input);
 
-        int counter = 0;
-        for(int i = 0; i < input.length()-12; i++) {
-            if(input.substring(i, i+13).equals("@softwire.com"))
-                counter++;
-        }
+        int counter = (int)matcher.results().count();
 
-        System.out.println("There are " + counter + " occurrences of @softwire.com in the sample text");
+        System.out.println("There are " + counter + " emails");
     }
     /*
-    * This program finds 301 occurrences of the substring @softwire.com, but this will generally over count
-    * the number of emails, as it also counts malformed emails, for example strings containing more than one @, and
-    * it will count domains that have softwire.com as a prefix, such as softwire.comet.com.
+    * This program finds 261 emails with the softwire.com domain.
     * */
 }
